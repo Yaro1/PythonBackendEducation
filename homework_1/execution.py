acc87f8cbed83905103cafac5382e21e50154c3e
y.poskryakov <y.poskryakov@corp.mail.ru>
2021-10-19 14:56:46 +0300

    homework_1: Made work

--- homework_1/execution.py
+++ homework_1/execution.py
@@ -1,6 +1,7 @@
 """
 Execution module for homework_1.
 """
+from homework_1.exceptions import BoardPutExceptionExists
 from homework_1.tic_tac_toe import Board
 
 
@@ -15,13 +16,19 @@ def execution():
     hello = "Please enter the name {number} player.\n"
     first_player, second_player = input(hello.format(number="first")), input(hello.format(number="second"))
     map_players = {'x': first_player, 'o': second_player}
-    size_board = int(input("Please enter the size board.\n"))
+    size_board = input("Please enter the size board.\n")
+    while not size_board.isdigit() or int(size_board) % 2 == 0:
+        size_board = input("Boardsize should be greater than 2 and odd\nTry again\nPlease enter the size board.\n")
+    size_board = int(size_board)
     board = Board(size_board)
     max_steps = size_board ** 2
     steps = 0
     while steps < max_steps:
         turn_to_walk = steps % 2 == 0
         step = input(f"Now goes player {first_player if turn_to_walk else second_player}\n").split()
+        while step[0] != 'p' or step[0] != 'q' or not step[0].isdigit() or not step[1].isdigit() or\
+            int(step[0]) < 0 or int(step[1]) < 0 or int(step[0]) >= board.size or int(step[1]) >= board.size:
+            step = input(f"Try again\nNow goes player {first_player if turn_to_walk else second_player}\n").split()
         if step[0] == 'p' or step[0] == 'q':
             if step[0] == 'p':
                 print(board)
@@ -29,7 +36,11 @@ def execution():
                 return 0
             continue
         coordinates = (int(step[0]), int(step[1]))
-        board.put(coordinates, 0 if turn_to_walk else 1)
+        try:
+            board.put(coordinates, 0 if turn_to_walk else 1)
+        except BoardPutExceptionExists:
+            print("This coordinates already employed.")
+            continue
         result = board.check_winner()
         if result != "nobody":
             print(f"The winner is {map_players[result]}.")
